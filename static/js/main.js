2ef8c95f3a6c983e6939e2631ffabc8c9766db20
lourduarte <lourdes123duarte@gmail.com>
2023-02-09 16:59:19 -0300

    changes

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<h1>Welcome to my <br> portfolio</h1><br><button class="btn-cv">CV</i></i></i></button>'	
+	],
+
+	typeSpeed: 200, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: 1, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
---
+++ static/js/scroll.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.item',{
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.item-buttom',{
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.contenido',{
+        duration: 2000,
+        origin: 'left',
+        distance:'400px',
+    });
+
+    sr.reveal('.contenido-right',{
+        duration: 2000,
+        origin: 'right',
+        distance:'400px',
+    });
+
