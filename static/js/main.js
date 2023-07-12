e89fceb5417fbf985a7e3f27698e6ec9ee5308fc
lourduarte <lourdes123duarte@gmail.com>
2023-07-12 01:49:42 -0300

    changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 const typed = new Typed('.typed', {
 	strings: [
-		'<p class="p-type"> { Hi! i&apos;m Lourdes,</p><span class ="span-type"> Welcome to my portfolio } </span><br><button class="btn-type">VIEW CV</button><br>'	
+		'<p class="p-type"> { Hi! i&apos;m Lourdes,</p><span class ="span-type"> Welcome to my portfolio } </span><br><a href="https://drive.google.com/file/d/1-zRox4P3s-0XzBglpJ_Dh1XP2bH6SK2a/view?usp=sharing" target="_blank"><button class="btn-type">VIEW CV</button></a><br>'	
 	],
 
 	typeSpeed: 200, // Velocidad en mlisegundos para poner una letra,
