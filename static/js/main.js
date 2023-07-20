1a1ccc4b9e9ea5d5c6871c7e6244ebfb4857e1a1
Lourdes <66228399+lourdDuarte@users.noreply.github.com>
2023-07-20 12:47:42 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,12 @@ const typed = new Typed('.typed', {
 		'<p class="p-type"> { Hi! i&apos;m Lourdes,</p><span class ="span-type"> Welcome to my portfolio } </span><br><a href="https://drive.google.com/file/d/1-zRox4P3s-0XzBglpJ_Dh1XP2bH6SK2a/view?usp=sharing" target="_blank"><button class="btn-type">VIEW CV</button></a><br>'	
 	],
 
-	typeSpeed: 200, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 1000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 0, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	backDelay: 1100, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: true, // Repetir el array de strings
 	loopCount: 1, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: false, // Mostrar cursor palpitanto
